225995ed5ca7df30370afb423cf709d4497dc0e3
matansharon <sharonmatan93@gmail.com>
2020-02-05 16:25:44 +0200

    Step 1: This commit will add a models - artist, song and facts, and also make test for them in test.py

--- facts/models.py
+++ facts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.PROTECT)
+    title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+
+class Fact(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.PROTECT)
+    content = models.TextField()
+    writtenBy = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"{self.content} \n נכתב על ידי {self.writtenBy}"
+
+
--- facts/tests.py
+++ facts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Artist, Song, Fact
 
-# Create your tests here.
+
+class FactTests(TestCase):
+    def test_create_items(self):
+        mashina = Artist.objects.create(
+            name='mashina',
+        )
+        train = mashina.song_set.create(
+            title='train to cairo'
+        )
+        train.fact_set.create(
+            content="great song!",
+            writtenBy="mashina",
+        )
